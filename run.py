456f49002e43f33e0c9e81939d39a5e637e986fa
AzureAD\AliceHumpage <alice.humpage@auroraer.com>
2026-08-28 14:26:10 +0100

    battery with better examples

--- run.py
+++ run.py
@@ -2,13 +2,13 @@ from pathlib import Path
 import subprocess
 import sys
 
-PLANTS = "plants_ramping.csv"
+PLANTS = "plants_basic.csv"
 FUEL = "fuel.csv"
 DEMAND = "demand.csv"
 PROFILES = "profiles_renewables.csv"
 USE_BATTERY = False
 BATTERY = "battery.csv"
-OUTPUT_NAME = "output_ramping"
+OUTPUT_NAME = "output_profiles"
 
 ROOT = Path(__file__).resolve().parent
 
